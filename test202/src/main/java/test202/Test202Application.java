268e76b270a7a6390ee0c2972e8cd8678b80a503
abdurrahimk <abdurrahimk@ABDURRAHIM>
2022-05-14 14:30:37 -0500

    first commit

---
+++ test202/src/main/java/test202/Test202Application.java
@@ -0,0 +1,14 @@
+package test202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test202Application.class, args);
+		System.out.println(8887);
+	}
+
+}
---
+++ test202/src/test/java/test202/Test202ApplicationTests.java
@@ -0,0 +1,13 @@
+package test202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
